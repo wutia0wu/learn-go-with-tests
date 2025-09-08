76989c4ecdd49659d0502a5957253dafc65c8022
sapphire <sukula.lumingze@gmail.com>
2025-09-08 12:16:54 +0800

    chapter 04 array_slice

--- array_slice/sum.go
+++ array_slice/sum.go
@@ -7,3 +7,34 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	result := make([]int, 0, lengthOfNumbers)
+
+	for _, numbers := range numbersToSum {
+		result = append(result, Sum(numbers))
+	}
+
+	return result
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+
+	// lengthOfNumbers := len(numbersToSum)
+	// result := make([]int, 0, lengthOfNumbers)
+
+	var result []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			result = append(result, 0)
+		} else {
+			// 注意 empty slice 将会导致越界引发 panic
+			tail := numbers[1:]
+			result = append(result, Sum(tail))
+		}
+	}
+
+	return result
+}
--- array_slice/sum_test.go
+++ array_slice/sum_test.go
@@ -1,18 +1,24 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
+
+	check := func(t *testing.T, got, want int, numbers []int) {
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	}
 
 	t.Run("collection of 5 numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		want := 15
-
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
+		check(t, got, want, numbers)
 	})
 
 	t.Run("collection of any size", func(t *testing.T) {
@@ -20,10 +26,42 @@ func TestSum(t *testing.T) {
 
 		got := Sum(numbers)
 		want := 6
-
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
+		check(t, got, want, numbers)
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{3, 4, 5})
+	want := []int{3, 12}
+
+	// reflect.DeepEqual 并非类型安全的
+	// want := "bob"  // 可以通过编译
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checke := func(t *testing.T, got, want []int) {
+		// reflect.DeepEqual 并非类型安全的
+		// want := "bob"  // 可以通过编译
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{3, 4, 5})
+		want := []int{2, 9}
+		checke(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checke(t, got, want)
+	})
 }
