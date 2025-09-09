eae3e75de765706e77e4df8656413517f5d9b657
sapphire <sukula.lumingze@gmail.com>
2025-09-09 23:00:33 +0800

    methods & interface

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -1,14 +1,34 @@
 package main
 
+import "math"
+
+// interface
+type Shape interface {
+	Area() float64
+}
+
+// 结构体的实现
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
+func (rectangle Rectangle) Perimeter() float64 {
 	return 2 * (rectangle.Height + rectangle.Width)
 }
 
-func Area(rectangle Rectangle) float64 {
+func (rectangle Rectangle) Area() float64 {
 	return rectangle.Height * rectangle.Width
+}
+
+type Circles struct {
+	Radius float64
+}
+
+func (circles Circles) Perimeter() float64 {
+	return 2 * math.Pi * circles.Radius
+}
+
+func (circles Circles) Area() float64 {
+	return math.Pi * circles.Radius * circles.Radius
 }
