0b8de6aba824e0fb64e84b113b8f12f7b21b9a2c
sapphire <sukula.lumingze@gmail.com>
2025-09-22 16:34:55 +0800

    chapter10 Select: httptest use Select

---
+++ select/racer_test.go
@@ -0,0 +1,35 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+
+	slowServer := makeDelayedServer(60 * time.Microsecond)
+	fastServer := makeDelayedServer(0 * time.Microsecond)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("slow: %s fast: %s \nwant: %s  got: %s", slowURL, fastURL, want, got)
+	}
+
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
