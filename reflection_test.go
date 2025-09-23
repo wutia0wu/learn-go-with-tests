094f64f41bd27311786a3489308514197f722bd6
sapphire <sukula.lumingze@gmail.com>
2025-09-23 21:37:15 +0800

    v0.2 TableTest

--- reflection_test.go
+++ reflection_test.go
@@ -3,23 +3,37 @@ package reflction
 import "testing"
 
 func TestWalk(t *testing.T) {
-	expected := "Truman"
-	var got []string
 
-	x := struct {
-		Name string
-	}{expected}
-
-	Walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one field",
+			struct {
+				Name string
+			}{"Truman"},
+			[]string{"Truman"},
+		},
 	}
 
-	if got[0] != expected {
-		t.Errorf("got %s want %s", got[0], expected)
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			Walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if len(got) != 1 {
+				t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+			}
+
+			if got[0] != test.ExpectedCalls[0] {
+				t.Errorf("got %s want %s", got[0], test.ExpectedCalls[0])
+			}
+		})
+
 	}
 
 }
